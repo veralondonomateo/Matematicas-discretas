95fdf214c93b6713a4a7e128ab943c5412007e8a
mateovera <veralondonomateo@gmail.com>
2023-10-23 11:03:23 -0500

    new commit

--- learnintento.py
+++ learnintento.py
@@ -7,20 +7,46 @@ import time
 pygame.init()
 
 # Configuración de la pantalla
-width, height = 820, 400
+width, height = 838, 600
 screen = pygame.display.set_mode((width, height))
 pygame.display.set_caption("Carrera de Atletismo en Pygame")
 
 # Colores
-white = (255, 255, 255)
+white = (245, 235, 225)
 red = (255, 0, 0)
 
 # Fuente para la alerta
 font = pygame.font.Font(None, 36)
 text_color = (0, 0, 0)
 
-runner1_image = pygame.image.load("/Users/mateovera/Documents/Programación/Python_intermediate/discretas/Flag_of_Jamaica.svg.png")
-runner1_image = pygame.transform.scale(runner1_image, (20, 20))
+
+
+estadosunidos = pygame.image.load("/Users/mateovera/Documents/Programación/Python_intermediate/discretas/172959-flag-usa-png-free-photo.png")
+estadosunidos = pygame.transform.scale(estadosunidos, (20, 20))
+
+francia = pygame.image.load("/Users/mateovera/Documents/Programación/Python_intermediate/discretas/Flag_of_France.svg.png")
+francia = pygame.transform.scale(francia,(20,20))
+
+jamaica = pygame.image.load("/Users/mateovera/Documents/Programación/Python_intermediate/discretas/Flag_of_Jamaica.svg.png")
+jamaica = pygame.transform.scale(jamaica, (20, 20))
+
+etiopia = pygame.image.load("/Users/mateovera/Documents/Programación/Python_intermediate/discretas/Flag_of_Ethiopia_(1897–1974).svg.png")
+etiopia = pygame.transform.scale(etiopia, (20, 20))
+
+polonia = pygame.image.load("/Users/mateovera/Documents/Programación/Python_intermediate/discretas/Flag_of_Poland.svg.png")
+polonia = pygame.transform.scale(polonia, (20, 20))
+
+Holanda = pygame.image.load("/Users/mateovera/Documents/Programación/Python_intermediate/discretas/Flag_of_the_Netherlands.svg.webp")
+Holanda = pygame.transform.scale(Holanda,(20,20))
+
+belgica = pygame.image.load("/Users/mateovera/Documents/Programación/Python_intermediate/discretas/Flag_of_Belgium.svg.png")
+belgica = pygame.transform.scale(belgica, (20, 20))
+
+kenia = pygame.image.load("/Users/mateovera/Documents/Programación/Python_intermediate/discretas/kENIA.png")
+kenia = pygame.transform.scale(kenia, (20, 20))
+
+#Velocidad
+velocidad = 20
 
 #Clase Numero1
 class Runner1:
@@ -29,7 +55,7 @@ class Runner1:
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
-        self.speed = 1 # Velocidad aleatoria
+        self.speed = (velocidad * 0.20) # Velocidad aleatoria
 
     def move(self):
         self.rect.x += self.speed
@@ -41,7 +67,7 @@ class Runner2:
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
-        self.speed = 2 # Velocidad aleatoria
+        self.speed = (velocidad * 0.05) # Velocidad aleatoria
 
     def move(self):
         self.rect.x += self.speed
@@ -54,7 +80,7 @@ class Runner3:
             self.rect = self.image.get_rect()
             self.rect.x = x
             self.rect.y = y
-            self.speed = 5 # Velocidad aleatoria
+            self.speed = (velocidad * 0.15) # Velocidad aleatoria
 
         def move(self):
             self.rect.x += self.speed
@@ -67,7 +93,7 @@ class Runner4:
             self.rect = self.image.get_rect()
             self.rect.x = x
             self.rect.y = y
-            self.speed = 2.5 # Velocidad aleatoria
+            self.speed = (velocidad * 0.18) # Velocidad aleatoria
 
         def move(self):
             self.rect.x += self.speed
@@ -80,7 +106,7 @@ class Runner5:
             self.rect = self.image.get_rect()
             self.rect.x = x
             self.rect.y = y
-            self.speed = 4 # Velocidad aleatoria
+            self.speed = (velocidad * 0.13) # Velocidad aleatoria
 
         def move(self):
             self.rect.x += self.speed
@@ -93,7 +119,7 @@ class Runner6:
             self.rect = self.image.get_rect()
             self.rect.x = x
             self.rect.y = y
-            self.speed = 3 # Velocidad aleatoria
+            self.speed = (velocidad * 0.10) # Velocidad aleatoria
 
         def move(self):
             self.rect.x += self.speed
@@ -106,7 +132,7 @@ class Runner7:
             self.rect = self.image.get_rect()
             self.rect.x = x
             self.rect.y = y
-            self.speed = 2 # Velocidad aleatoria
+            self.speed = (velocidad * 0.07) # Velocidad aleatoria
 
         def move(self):
             self.rect.x += self.speed
@@ -119,64 +145,64 @@ class Runner8:
             self.rect = self.image.get_rect()
             self.rect.x = x
             self.rect.y = y
-            self.speed = 1 # Velocidad aleatoria
+            self.speed = velocidad * 0.12 # Velocidad aleatoria
 
         def move(self):
             self.rect.x += self.speed
 
 
 #Corredores1
-runners1 = [Runner1(runner1_image, 0 , 40 )]
-runners2 = [Runner1(runner1_image, 200 , 40)]
-runners3 = [Runner1(runner1_image, 400 , 40)]
-runners4 = [Runner1(runner1_image, 600 ,40)]
+runners1 = [Runner1(estadosunidos, 0 , 40 )]
+runners2 = [Runner1(estadosunidos, 200 , 40)]
+runners3 = [Runner1(estadosunidos, 400 , 40)]
+runners4 = [Runner1(estadosunidos, 600 ,40)]
 
 #Corredores2
-runners5 = [Runner2(runner1_image, 0 , 80 )]
-runners6 = [Runner2(runner1_image, 200 , 80)]
-runners7 = [Runner2(runner1_image, 400 , 80)]
-runners8 = [Runner2(runner1_image, 600 , 80)]
+runners5 = [Runner2(francia, 0 , 80 )]
+runners6 = [Runner2(francia, 200 , 80)]
+runners7 = [Runner2(francia, 400 , 80)]
+runners8 = [Runner2(francia, 600 , 80)]
 
 #Corredores3
-runners9 = [Runner3(runner1_image, 0 , 120 )]
-runners10 = [Runner3(runner1_image, 200 , 120)]
-runners11 = [Runner3(runner1_image, 400 , 120)]
-runners12 = [Runner3(runner1_image, 600 , 120)]
+runners9 = [Runner3(jamaica, 0 , 120 )]
+runners10 = [Runner3(jamaica, 200 , 120)]
+runners11 = [Runner3(jamaica, 400 , 120)]
+runners12 = [Runner3(jamaica, 600 , 120)]
 
 #Corredores4
 
-runners13 = [Runner4(runner1_image, 0 , 160 )]
-runners14 = [Runner4(runner1_image, 200 , 160)]
-runners15 = [Runner4(runner1_image, 400 , 160)]
-runners16 = [Runner4(runner1_image, 600 , 160)]
+runners13 = [Runner4(etiopia, 0 , 160 )]
+runners14 = [Runner4(etiopia, 200 , 160)]
+runners15 = [Runner4(etiopia, 400 , 160)]
+runners16 = [Runner4(etiopia, 600 , 160)]
 
 #Corredores5
 
-runners17 = [Runner5(runner1_image, 0 , 200 )]
-runners18 = [Runner5(runner1_image, 200 , 200)]
-runners19 = [Runner5(runner1_image, 400 , 200)]
-runners20 = [Runner5(runner1_image, 600 , 200)]
+runners17 = [Runner5(polonia, 0 , 200 )]
+runners18 = [Runner5(polonia, 200 , 200)]
+runners19 = [Runner5(polonia, 400 , 200)]
+runners20 = [Runner5(polonia, 600 , 200)]
 
 #Corredores6
 
-runners21 = [Runner6(runner1_image, 0 , 240 )]
-runners22 = [Runner6(runner1_image, 200 , 240)]
-runners23 = [Runner6(runner1_image, 400 , 240)]
-runners24 = [Runner6(runner1_image, 600 , 240)]
+runners21 = [Runner6(Holanda, 0 , 240 )]
+runners22 = [Runner6(Holanda, 200 , 240)]
+runners23 = [Runner6(Holanda, 400 , 240)]
+runners24 = [Runner6(Holanda, 600 , 240)]
 
 #Corredores7
 
-runners25 = [Runner7(runner1_image, 0 , 280 )]
-runners26 = [Runner7(runner1_image, 200 , 280)]
-runners27 = [Runner7(runner1_image, 400 , 280)]
-runners28 = [Runner7(runner1_image, 600 , 280)]
+runners25 = [Runner7(belgica, 0 , 280 )]
+runners26 = [Runner7(belgica, 200 , 280)]
+runners27 = [Runner7(belgica, 400 , 280)]
+runners28 = [Runner7(belgica, 600 , 280)]
 
 #Corredores8
 
-runners29 = [Runner8(runner1_image, 0 , 320 )]
-runners30 = [Runner8(runner1_image, 200 , 320)]
-runners31 = [Runner8(runner1_image, 400 ,320)]
-runners32 = [Runner8(runner1_image, 600 , 320)]
+runners29 = [Runner8(kenia, 0 , 320 )]
+runners30 = [Runner8(kenia, 200 , 320)]
+runners31 = [Runner8(kenia, 400 ,320)]
+runners32 = [Runner8(kenia, 600 , 320)]
 
 
 # Bucle principal
@@ -189,6 +215,8 @@ clock = pygame.time.Clock()
 
 
 while running:
+    
+
     x = False
     y = False
     z = False
@@ -247,8 +275,16 @@ while running:
             # Verifica si un corredor ha llegado a la línea de meta
             finish_line = 780
             if runner.rect.x >= finish_line and winner is None:
-                winner = runner
+                runner.speed = 0
+
+                #Oro Plata Y Bronce
+                oro = "Estados unidos"
+                plata = " Etiopía"
+                bronce = "Jamaica"
+                text = font.render(f' Oro para {oro}   Plata para {plata}   Bronce {bronce}', True, text_color)
+                
                 ganador = True
+
 
 #Corredores Equipo2
 
@@ -300,7 +336,7 @@ while running:
             # Verifica si un corredor ha llegado a la línea de meta
             finish_line = 780
             if runner.rect.x >= finish_line and winner is None:
-                runner.speed = 0
+                winner = runner
                 ganador = True
 
 #Corredores Equipo3
@@ -316,7 +352,6 @@ while running:
                 runner.speed = 0
                 x = True
         
-
     if game_started:
         # Actualiza la posición de los corredores de manera animada
         if x:
@@ -354,10 +389,9 @@ while running:
             finish_line = 780
             if runner.rect.x >= finish_line and winner is None:
                 runner.speed = 0
-                
-                paisganador = "Jamaica"
-                text = font.render(f'El ganador es {paisganador}', True, text_color)
-
+ 
+         #       paisganador = "Jamaica"
+        #        text = font.render(f'El ganador es {paisganador}', True, text_color)
                 ganador = True
 
 
@@ -723,8 +757,19 @@ while running:
 
     # Si hay un ganador, muestra una alerta
     if ganador:
-        screen.blit(text, (300, 300))
+        screen.blit(text, (70, 410))
 
+    text_surface = font.render("400m", True, text_color)
+    screen.blit(text_surface, (163, 350))
+
+    text_surface = font.render("800m", True, text_color)
+    screen.blit(text_surface, (363, 350))
+
+    text_surface = font.render("1200m", True, text_color)
+    screen.blit(text_surface, (563, 350))
+
+    text_surface = font.render("1600m", True, text_color)
+    screen.blit(text_surface, (760, 350))
 
     # Actualiza la pantalla
     pygame.display.flip()
@@ -734,6 +779,7 @@ while running:
     # Si hay un ganador, detén el juego
    
     if winner:
+        time.sleep(10)
         running = False
 
 # Cierra Pygame
